480132c270afe83c9207007c8b76be272bbdf062
julio burgoin <julio_burgoin@outlook.com>
2024-04-12 16:56:01 -0600

    subida

---
+++ Paginaweb/php/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Página</title>
+</head>
+<body>
+    <h1>Página Principal</h1>
+    <a href="inventario.php"><button>Inventario de bodegas</button></a>
+</body>
+</html>
---
+++ Paginaweb/php/inventario.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventario de Bodegas</title>
+</head>
+<body>
+    <h1>Inventario de Bodegas</h1>
+    <a href="registrar.php"><button>Registrar Nueva Renta</button></a>
+    <a href="bodegasrentadas.php"><button>Ver Bodegas Rentadas</button></a>
+</body>
+</html>
