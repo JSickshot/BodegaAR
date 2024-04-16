220d454207ef2e85824090c17d968ceb1937cffb
julio burgoin <julio_burgoin@outlook.com>
2024-04-16 11:55:41 -0600

    subid

---
+++ Paginaweb/php/bodegasrentadas.php
@@ -0,0 +1,1 @@
+<a href="../php/inventario.php" class="regresar-button">Regresar</a>
--- Paginaweb/php/registrar.php
+++ Paginaweb/php/registrar.php
@@ -1,11 +1,44 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
+
 <head>
+    <title>Bitacora de mantenimiento</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi Página</title>
+    <link rel="stylesheet" href="../css/bita.css">
 </head>
+
 <body>
-    
+            <h2>Registre al usuario</h2>
+
+                <div class="form-group">
+                    <label for="fecha">FECHA:</label>
+                    <input type="date" name="fecha" id="fecha" required readonly>
+                </div>
+
+                <label for="Primer">Primero apellido:</label>
+                <input type="text" name="Nombre" required>
+                <br>
+
+                <label for="segundo">Segundo apellido:</label>
+                <input type="text" name="Nombre" required>
+
+                <label for="Nombres">Nombres:</label>
+                <input type="text" name="Nombre" required>
+                <br>
+
+                <label for="Correo">Correo de contacto: </label>
+                <input type="text" name="Nombre" required>
+
+                <label for="Bodega">Bodega a rentar</label>
+                <input type="text" name="Nombre" required>
+                <br>
+
+                <br>
+                </br>
+
+
+                <a href="../php/inventario.php" class="regresar-button">Regresar</a>
+
 </body>
+
 </html>
